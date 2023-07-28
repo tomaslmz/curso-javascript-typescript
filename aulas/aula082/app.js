0f079492269ca7470ec53c019e30abd3efe1cc71
tomas <tomas153lm@gmail.com>
2023-07-28 10:02:43 -0300

    finalizacao: aula 82

---
+++ aulas/aula082/app.js
@@ -0,0 +1,28 @@
+// const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Júlia'];
+
+// Remove o último nome
+// nomes.pop();
+
+// Remove o primeiro nome
+// nomes.shift();
+
+// Desloca todos os valores e adiciona ao primeiro lugar do array
+// nomes.unshift("Lucas");
+
+// Adiciona um valor ao último lugar do array
+// nomes.push("Tomás");
+
+// Corta um array conforme os parâmetros
+// const novosNomes = nomes.slice(0, -1);
+
+
+// const nome = "Luiz Otávio Miranda";
+//Divide uma string e transforma em um array;
+// const nomes = nome.split(" ");
+
+const nomes = ["Luiz", "Otávio", "Miranda"];
+//Junta os arrays;
+const nome = nomes.join(" ")
+
+console.log(nome);
+// console.log(novosNomes);

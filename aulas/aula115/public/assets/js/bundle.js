15b9df4df7dc014200b2c3665d4ae91e02abd1e8
tomaslmz <tomas153lm@gmail.com>
2023-09-04 16:21:45 -0300

    finalizacao da aula 115

---
+++ aulas/aula115/public/assets/js/bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+var msg = 'Hello world!';
+alert(msg);
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
